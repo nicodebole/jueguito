5418fe263906ac91e96ca3da0a3ae17b13e48365
Nicolas <nico.debole95@gmail.com>
2022-01-25 22:47:48 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Cuadricula from './components/cuadricula/Cuadricula';
 
 function App() {
-  // const [ chance, setChance ] = useState('');
-  // const [ fila, setFila ] = useState(0);
   const [datos, setDatos] = useState([]);
+  const [ronda, setRonda] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const [win, setWin] = useState(false);
 
-  // useEffect(() => {
-  //   console.log(chance);
-  //   console.log(objCua)
-  // }, [chance])
+
+  useEffect(() => {
+    if(datos[datos.length-1] === 'GATOS')
+      setWin(true);
+  }, [datos])
+
+  useEffect(() => {
+    if (ronda === 6 || win)
+      setGameOver(true);
+  }, [ronda, win])
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
 
   return (
     <div>
-      <Cuadricula palabras={datos}></Cuadricula>
-      <input id='palabra'></input>
-      <button onClick={() => setDatos(prev => ([...prev, document.getElementById('palabra').value]))}>Click</button>
+      {
+        !gameOver && <div className='partida-container'>
+          <Cuadricula palabras={datos}></Cuadricula>
+          <div className='search'>
+            <input id='palabra'></input>
+            <button onClick={() => 
+              document.getElementById('palabra').value.length === 5 ? setDatos(prev => ([...prev, document.getElementById('palabra').value.toUpperCase()])) & setRonda(prev => prev + 1) : alert('Pone 5 letritas, dale')}>Click</button>
+          </div>
+        </div>
+        ||
+        <div className='gameover-container'>
+          <h1>GAME OVER</h1>
+          {win && <h1>GANASTE</h1>}
+          <button onClick={refreshPage}>JUGAR DE NUEVO</button>
+        </div>
+      }
     </div>
   );
 }
--- src/components/fila/Fila.jsx
+++ src/components/fila/Fila.jsx
@@ -2,16 +2,20 @@ import './fila.css';
 import $ from 'jquery';
 
 const Fila = ({ palabra, index }) => {
-    const ASACAR = 'gatos';
+    const ASACAR = 'GATOS';
 
-    const jq = (i) => {
-        $(`#${index}-${i}`).css('background-color', palabra[i] === ASACAR[i] ? 'green' : 'blue');
+    const jq = (i, isNot) => {
+        if (isNot)
+            $(`#${index}-${i}`).css('background-color', '#393839');
+        else
+            $(`#${index}-${i}`).css('background-color', palabra[i] === ASACAR[i] ? '#16c60c' : '#fff100');
     }
 
     const crearFila = () => {
         const result = [];
-        for(let i = 0; i < 5; i++) {
-            result.push(<div key={i} id={`${index}-${i}`} className="each">{palabra ? palabra[i] : null}{palabra ? (ASACAR.includes(palabra[i]) ? jq(i) : null) : null}</div>);
+        for (let i = 0; i < 5; i++) {
+            result.push(<div key={i} id={`${index}-${i}`} className="each">{palabra ? palabra[i] : null}
+                {palabra ? (ASACAR.includes(palabra[i]) ? jq(i) : jq(i, -1)) : null}</div>);
         }
 
         return result;
